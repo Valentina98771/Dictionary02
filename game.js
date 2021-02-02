eee448a79e063f47ad0a70e03ca8833701c60473
Valentina98771 <76867909+Valentina98771@users.noreply.github.com>
2021-02-02 10:14:16 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+let findWord, game, words = [];
+
+function myFunction() {
+    game = document.getElementById("b1").value;
+    words.push(game);
+    document.getElementById("b1").value = "";
+}
+
+function existWord() {
+    findWord = document.getElementById("b2").value;
+        if(words.includes(findWord)) {
+            document.getElementById("print").innerHTML = "The searched word can be found in the dictionary";
+            alert("The searched word can be found in the dictionary");
+        } else {
+            document.getElementById("print").innerHTML = "I'm sorry, I didn't find the word I was looking for in the dictionary"
+             alert("I'm sorry, I didn't find the word I was looking for in the dictionary");
+        }
+    
+}
